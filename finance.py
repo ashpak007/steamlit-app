7168876135220deee920281cc117437f09642f14
Ashpak <ashpaksheikh0801@gmail.com>
2026-02-02 21:01:10 +0530

    Changes to a financial App

--- finance.py
+++ finance.py
@@ -2,13 +2,53 @@
 
 import yfinance as yf
 import streamlit as st
+import pandas as pd
+import datetime
 
-ticker_symbol = 'AAPL'
+st.write( """
+           #This is my heading 
+         
+           This is some description about my app
+
+            """ )
+
+col1, col2 = st.columns(2)
+
+with col1:
+    start_date = st.date_input("Please enter Starting Date",
+                  datetime.date(2019,1,1))
+
+with col2:
+    end_date = st.date_input("Please enter End Date",
+                  datetime.date(2022,12,31))
+
+
+ticker_symbol = st.text_input("Enter Stock Symbol",
+                              "AAPL",
+                              key="placeholder")
 
 ticker_data = yf.Ticker(ticker_symbol)
+ticker_df = ticker_data.history(start=f"{start_date}",
+                                end=f"{end_date}")
 
-ticker_df = ticker_data.history(start = '2023-01-01',
-                                end = '2025-01-01',
-                                interval = '1d' )
 
-st.dataframe(ticker_df)
+st.write(
+    f"""
+       ## {ticker_symbol}'s EOD Price 
+    """
+)
+st.dataframe(ticker_df) 
+
+st.write(
+    """
+       ## Daily Closing Price Chart
+    """
+)
+st.line_chart(ticker_df.Close)
+
+st.write(
+    """
+        ## Volume of Shares Traded Each Day
+    """
+)
+st.line_chart(ticker_df.Volume)
